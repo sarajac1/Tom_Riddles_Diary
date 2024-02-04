fe219e1f74b82136aa63e23b878599819260d2c8
sarajac1 <sarajac@hotmail.com>
2024-02-04 20:21:15 +0100

    Attempting to create a flow of conversation with the chatbot and different outcomes based on different replies. Not working right now but committing to save progress.

--- diary.py
+++ diary.py
@@ -1,8 +1,15 @@
 # Importerar SpaCy
 import spacy
 
+#Importerar PhraseMatcher
+from spacy.matcher import PhraseMatcher
+
 # Ladda SpaCy modellen med engelska språkpaketet
 nlp = spacy.load("en_core_web_lg")
+
+#Ställer in språkinställningar, case-insensitive
+matcher = PhraseMatcher(nlp.vocab, attr="LOWER")
+
 
 # Funktion för att få användaren att mata in sitt namn (eller annat korrekt namn)
 def get_name(prompt):
@@ -11,26 +18,29 @@ def get_name(prompt):
         user_input = input(prompt)
         #Skapar ett Doc objekt av användarens inmatning
         doc = nlp(user_input)
-        #Skapar en variabel och ger den värdet falskt
-        person_found = False
+        #Skapar en boole och ger den värdet falskt
+        name_found = False
 
         #Loopar genom entiteterna i doc
         for ent in doc.ents:
             # Om input är en entitet av typen "person" så kommer personligt meddelande skrivas ut
             if ent.label_ == "PERSON":
                 #Variabeln blir sann då ett namn hittats
-                person_found = True
+                name_found = True
                 #Personligt meddelande skrivs ut och loopen bryts
                 print(f"Tom Riddle: Hello {ent.text}. My name is Tom Riddle. How did you come by my app?")
                 break
 
         #Loopen bryts om namn hittats
-        if person_found:
+        if name_found:
             break
         #Om SpaCy inte känner igen det inmatade värdet som ett namn så kommer en uppmaning att uppge ett riktigt namn
         else:
-            print("Tom Riddle: I don't recognize that as a name. I can tell when people are lying to me. I will ask "
+            print("Tom Riddle: I can tell when people are lying to me and that is not a real name. I will ask "
                   "you again. What is your name?")
+
+# Ber användaren skriva in sitt namn
+get_name("What is your name? ")
 
 
 # Funktion för att skriva ut ett svar baserat på användarens inmatning
@@ -38,9 +48,47 @@ def get_name(prompt):
 #     response = respond_name(user_input)
 #     return response
 
+def get_user_response(prompt):
+    user_input = input(prompt)
+    doc = nlp(user_input)
+    matches = matcher(doc)
+    return doc, matches
 
-# Ber användaren skriva in sitt namn
-user_input = get_name("What is your name? ")
+first_reply_keywords = ["github", "computer", "online"]
+patterns = [nlp.make_doc(text) for text in first_reply_keywords]
+matcher.add("FirstReplyKeywords", None, *patterns)
+
+def respond_to_first_reply(doc):
+    matches = matcher(doc)
+    for match_id, start, end in matches:
+        span = doc[start:end]
+        if span.text.lower() in ("github", "computer", "online"):
+                return ("(Tom Riddle: Lucky that I recorded my memories in some more lasting way than ink. But I always "
+                        "knew that there would be those who would not want this read.")
+        return ("Tom Riddle: Is that some muggle technology? Anyway, it was lucky that  I recorded my memories in some"
+                "more lasting way than ink. But I always knew that there would be those who would not want this "
+                "read. Would you like me to tell you more?")
+
+
+
+
+
+
+
+
+
+
+
+
+
+def chat():
+    user_name_input = get_name("What is your name? ")
+    response = respond_to_first_reply(user_name_input)
+    print("Tom Riddle: ", response)
+
+while True:
+    user_input = get_name("")
+    doc = nlp(user_input.text)
 
 # #Anropar funktionen som genererar ett svar
 # response = generate_response(user_input)
