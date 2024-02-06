4e9939ae908adc7a27a7460072e4252010f60bc1
sarajac1 <sarajac@hotmail.com>
2024-02-06 18:49:38 +0100

    Updated readme, added answers in facit.txt, changed som functionality in the app

--- diary.py
+++ diary.py
@@ -1,147 +1,203 @@
 # Importerar SpaCy
 import spacy
 
-#Importerar PhraseMatcher
+# Importerar PhraseMatcher
 from spacy.matcher import PhraseMatcher
 
 # Ladda SpaCy modellen med engelska språkpaketet
 nlp = spacy.load("en_core_web_lg")
 
-#Ställer in språkinställningar, case-insensitive
+# Ställer in språkinställningar, case-insensitive
 matcher = PhraseMatcher(nlp.vocab, attr="LOWER")
+
 
 # Funktion för att få användaren att mata in sitt namn (eller annat korrekt namn)
 def get_name(prompt):
+    while True:
         # Sparar värdet av användarens input
         user_input = input(prompt)
-        #Skapar ett Doc objekt av användarens inmatning
+        # Skapar ett Doc objekt av användarens inmatning
         doc = nlp(user_input)
 
-        #Loopar genom entiteterna i doc
+        # Om användarens input (case-insensitive) innehåller någon av orden nedan så skrivs ett meddelande ut och
+        # applikationen avslutas
+        if user_input.lower() in ["exit", "quit", "stop", "no"]:
+            print("You are not worthy of this app, muggle. It is now closing down.")
+            return
+
+        # Loopar genom entiteterna i doc
         for ent in doc.ents:
             # Om input är en entitet av typen "person" så kommer personligt meddelande skrivas ut
             # _(understreck) här gör så att det är sträng-värdet som hanteras och inget numeriskt värde
             if ent.label_ == "PERSON":
-                #Personligt meddelande skrivs ut och loopen bryts
+                # Personligt meddelande skrivs ut och loopen bryts
                 print(f"Tom Riddle: Hello {ent.text}. My name is Tom Riddle.")
-                #Returnererar namnet som hämtats
+                # Returnerar namnet som hämtats
                 return ent.text
-        #Om SpaCy inte känner igen det inmatade värdet som ett namn så kommer en uppmaning att uppge ett riktigt namn
-            print("Tom Riddle: I can tell when people are lying to me and that is not a real name. I will ask "
-                  "you again. What is your name?")
+            else:
+                # Om SpaCy inte känner igen entiteten som ett namn så kommer en uppmaning att uppge ett riktigt namn
+                print("Tom Riddle: I know when people are lying to me and that is not a real name.\n I will ask"
+                      "you again.")
 
-#Funktion som ska få användarens input
+
+# Funktion som ska få användarens input
 def get_user_response(prompt):
-    #Skapar variabel som användarens input sparas i
+    # Skapar variabel som användarens input sparas i
     user_input = input(prompt)
-    #Input blir ett doc objekt
+    if user_input.lower() in ["exit", "quit", "stop", "no"]:
+        print("You are not worthy of this app, muggle. It is now closing down.")
+        return
+    # Input blir ett doc objekt
     doc = nlp(user_input)
-    #Returnerar objektet doc
+    # Returnerar objektet doc
     return doc
 
-#Funktion för att matcha första nyckelorden
+
+# Funktion för att matcha första nyckelorden
 def first_matcher():
-    #Anger vilka nyckelord som ska matchas
+    # Anger vilka nyckelord som ska matchas
     keywords = ["github", "computer", "online"]
-    #Loopar över varje ord i nyckelorden och skapar doc objekt av deras tokens
+    # Loopar över varje ord i nyckelorden och skapar doc objekt av deras tokens
     patterns = [nlp.make_doc(text) for text in keywords]
-    #Lägger till pattern ovan till matcher. * i patterns gör så att varje objekt
-    #hanteras för sig
-    matcher.add("FirstReplyWords", None, *patterns)
+    # Lägger till pattern ovan till matcher.
+    matcher.add("FirstReplyKeywords", patterns)
 
 
-#Funktion som svarar på första matchningen
+# Funktion som svarar på första matchningen
 def respond_to_first_reply(doc):
-    #Skapar en variabel av innehållet i doc objektet
+    # Skapar en variabel av innehållet i doc objektet
     matches = matcher(doc)
-    #Skapar en variabel för att hålla koll på om matchning hittats, initialiseras till falskt
+    # Skapar en variabel för att hålla koll på om matchning hittats, initialiseras till falskt
     match_found = False
-    #Loopar genom varje matchning och hittar deras plats
+    # Loopar genom varje matchning och hittar deras plats
     for match_id, start, end in matches:
-        #Tar ut det spannet from dokumentet som matchar sökningen
-        span = doc[start:end]
-        if (span.text.lower() in ["github", "computer", "online"]):
+        if nlp.vocab.strings[match_id] == "FirstReplyKeywords":
+            # Loopen avbryts när matchning hittats
+            match_found = True
+            break
+
+    # Om matchning hittats så skrivs passande meddelande ut
+    if match_found:
+        return ("Lucky that I recorded my memories in some more lasting way than ink.\n"
+                "This app holds memories of terrible things involving Hogwarts and the Chamber of Secrets.")
+    # Om ingen matchning hittats så skrivs ett annat meddelande ut där användaren fortsatt har möjlighet att fortsätta
+    else:
+        return ("Is that some muggle technology? Anyway, this app holds memories of terrible things involving "
+                "Hogwarts and the Chamber of Secrets.")
+
+
+# Funktion för att matcha fler nyckelord
+def second_matcher():
+    # Anger nyckelord att leta efter
+    keywords = ["yes", "tell me", "chamber of secrets"]
+    # Loopar över varje ord i nyckelorden och skapar doc objekt av deras tokens
+    patterns = [nlp.make_doc(text) for text in keywords]
+    # Lägger till i matcher
+    matcher.add("SecondReplyKeywords", patterns)
+
+
+# Funktion för att svara på användarens input
+def respond_to_second_reply(doc):
+    matches = matcher(doc)
+    match_found = False
+    for match_id, start, end in matches:
+        if nlp.vocab.strings[match_id] == "SecondReplyKeywords":
             match_found = True
             break
 
     if match_found:
-        return ("Lucky that I recorded my memories in some more lasting way than ink. But I always "
-                    "knew that there would be those who would not want this read.\n  Would you like to know why?")
+        return ("In my fifth year, the Chamber of Secrets was opened and a monster attacked several students,\n"
+                "finally killing one. I caught the person who had opened the Chamber and he was expelled. \n"
+                "The story was covered up as an accident and I was warned to keep my mouth shut. \n"
+                "But the monster is still alive and it can happen again.")
     else:
-        return ("Is that some muggle technology? Anyway, it was lucky that  I recorded my memories in some"
-                    "more lasting way than ink. \n But I always knew that there would be those who would not want this "
-                    "read. Would you like to know why?")
-
-
-#Funktion för att matcha fler nyckelord
-def second_matcher():
-    # Anger nyckelord att leta efter
-    keywords = ["yes", "tell me", "why?"]
-    # Loopar över varje ord i nyckelorden och skapar doc objekt av deras tokens
-    patterns = [nlp.make_doc(text) for text in keywords]
-    # Lägger till i matcher
-    matcher.add("SecondReplyWords", None, *patterns)
-
-#Funktion för att svara på användarens input
-def respond_to_second_reply(doc):
-    matches = matcher(doc)
-    for match_id, start, end in matches:
-        span = doc[start:end]
-        if span.text.lower() in ["github", "computer", "online"]:
-            return ("This app holds memories of terrible things. Things which were covered up. \n "
-                    "Things which happened at Hogwarts School of Witchcraft and Wizardry. Would you like to know more?")
         return "Are you not interested in the Chamber of Secrets?"
 
 
-#Funktion för tredje matchningen
-def third_matcher (doc):
-    keywords = ["chamber of secrets", "yes", "tell me"]
+# Funktion för tredje matchningen
+def third_matcher():
+    keywords = ["who", "so", "and"]
     patterns = [nlp.make_doc(text) for text in keywords]
-    matcher.add("ThirdReplyWords", None, *patterns)
+    matcher.add("ThirdReplyKeywords", patterns)
 
 
 def respond_to_third_reply(doc):
     matches = matcher(doc)
+    match_found = False
     for match_id, start, end in matches:
-        span = doc[start:end]
-        if span.text.lower() in ["chamber of secrets", "yes", "tell me"]:
-            return ("In my day they told me the Chamber of Secrets was a legend, that it did not exist.\n"
-                    "In my fifth year, the Chamber was opened and the monster attacked several students, finally \n"
-                    "killing one. I caught the person who had opened the Chamber and he was expelled. \n"
-                    "But the Headmaster, Professor Dippet, ashamed that such a thing had happened at Hogwarts, \n"
-                    "forbade me to tell the truth. A story was given out that the girl had died in a freak accident.\n"
-                    "They gave me a nice, shiny, engraved trophy for my trouble and warned me to keep my mouth shut. "
-                    "But I knew it could happen again. The monster lived on, and the one who had the power to release\n"
-                    "it was not imprisoned.'")
+        if nlp.vocab.strings[match_id] == "ThirdReplyKeywords":
+            match_found = True
+            break
+
+    if match_found:
+        return ("IT WAS ME ALL ALONG\n"
+                "THE HEIR OF SLYTHERIN HAS INFECTED YOUR COMPUTER WITH A VIRUS. *HISSES IN PARSELTONGUE* \n,"
+                """
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣄⣶⣶⣶⣶⣾⣿⣿⣿⣶⣶⣶⣦⣄⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⢰⣦⣄⣀⠀⠀⠀⠀⣀⣀⡀⠀⢀⣀⣤⣤⣴⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⡿⠿⣟⠻⠏⢩⢹⣿⣿⣿⣿⡿⠿⢿⣿⡿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⢀⣘⠻⠯⢑⣊⣉⣀⠀⠀⢸⡘⠛⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⡆⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠙⠛⠛⠿⢿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⡏⠀⣠⡾⠛⠻⣿⡿⠟⠃⠸⡇⢀⣴⣷⠀⠀⠀⠀⠀⠀⢀⣴⣧⡀⢹⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠻⢧⡀⠀⠀⠀⠀⠀
+        ⠀⠀⡀⠄⠒⡇⠀⣿⣦⡀⠀⠉⠀⠀⠀⠀⡇⠀⣿⣿⠀⠀⣀⠀⠀⣀⢨⣿⡏⠀⢸⣿⣧⣴⣦⡀⠀⣴⣶⣶⣄⠀⣀⣴⣤⣤⡀⠀⣿⠇⠀⠀⠀⠀⠈⠧⠤⢄⡀⠀
+        ⣠⠊⠀⠀⠀⠃⠀⠙⣿⣿⣶⣄⠀⠀⠀⠀⡇⠀⣿⣿⠘⣿⣿⠀⣿⡇⢸⣿⡇⠀⢸⣿⠃⢹⣿⡇⢸⣿⠁⣸⠿⠀⣿⣿⠛⠟⠁⣤⣴⡆⢀⣀⣠⡀⠀⠀⠀⠀⠉⡦
+        ⠈⠢⣤⠀⠀⢸⠀⠀⠈⠻⣿⣿⣿⣦⡀⠀⠇⠀⣿⣿⠀⠸⣿⣦⣿⠁⠘⣿⣇⠄⣸⣿⡀⢸⣿⡇⢸⣿⡋⢀⡆⠀⣿⣿⠀⠀⢀⣿⡿⠀⢸⣿⡿⢻⣿⡆⠀⢀⠞⠁
+        ⠀⠀⠀⠰⡀⣸⣤⣤⣤⣄⠈⠻⣿⣿⣧⠀⠀⠀⠾⠿⠂⠀⢹⡿⠃⠀⠈⠛⠁⠈⠉⠁⠀⠚⠛⠓⠈⠛⠷⠛⠁⠠⠿⠿⠄⠀⣼⣿⡇⠀⣸⣿⠁⢸⣿⠇⢠⠋⠀⠀
+        ⠀⠀⠀⠀⣿⣿⠋⠁⠉⢿⣷⠀⠸⣿⣿⠀⢠⠀⠀⣴⣷⡾⠟⠁⣀⡤⠔⠒⠉⠉⠉⠉⠉⠓⠢⢄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠁⠠⠿⠇⢀⣾⣿⠀⢳⠀⠀⠀
+        ⠀⠀⠀⢸⣿⠁⠀⢦⣤⣾⡿⠀⢠⣿⣿⠂⢸⠀⣀⠠⣄⣒⢉⡿⠋⠀⠀⠀⢰⣄⣠⣀⣄⢀⠀⠀⠈⠣⣉⣲⢶⣤⣤⣀⣀⣀⠀⠀⠀⠀⠀⠀⠉⠙⠃⢀⡏⠀⠀⠀
+        ⠀⠀⠀⢹⣿⣆⠀⠀⠀⠀⠀⢀⣾⣿⠟⠀⠸⠉⢰⣿⣿⢏⡞⠀⠀⠀⣠⣾⢟⣻⣯⣭⣟⡻⢿⣄⠀⠀⢶⣶⣍⠚⠋⠉⢽⢻⣷⣦⣄⡀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠻⣿⣿⣶⣦⣴⡶⠟⠋⣁⣀⡤⠃⢀⣼⣿⡟⣼⡃⠀⠀⢰⡟⠀⠈⢻⡟⠁⠈⢻⣇⢿⡇⠀⣠⣤⠔⠒⠤⡂⢌⣿⣿⣿⣿⣿⣷⣦⣄⡀⠀⡜⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⡇⠀⠀⣀⠤⣒⣭⡍⢰⣾⣿⣿⣿⣿⣿⡇⡟⡁⠀⠀⢸⡇⣄⠰⣿⣿⠆⣴⣿⣿⣮⡳⡀⠹⡇⠐⠢⡔⣿⣿⣿⣿⡿⢿⣿⣿⣿⡇⠉⠙⠃⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠧⠒⠉⣶⣿⣿⣿⡇⠈⢿⣿⣿⣿⠿⢻⣇⢿⣷⠀⠀⠀⠙⢎⡳⣌⣡⣾⣿⣿⣿⣿⡇⠀⣥⣢⠅⣒⠛⣢⣽⡿⠋⠀⢸⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⡇⠀⣤⡙⣿⢱⢹⢸⣿⣌⢿⣷⣀⠀⠀⠀⠙⠢⢝⡻⢿⣿⣿⣿⣷⣠⣿⣿⣿⣶⣶⣿⣫⣶⣄⠀⢸⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⡇⢸⣿⣿⡌⢸⠀⣞⢿⣿⣷⣝⠿⣷⣦⣄⣀⡀⠀⠉⠐⠨⢝⡻⢿⣿⣿⣿⣿⣿⠟⠹⣿⣿⣿⠆⢸⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⡇⠀⢹⡟⡱⠟⣇⠘⢷⡒⢭⡻⣷⣮⣝⡻⣿⣿⣦⣀⠀⠀⠀⠉⠒⢭⡻⠿⢟⣅⠀⠀⣹⣿⡁⠀⢸⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⡇⢠⡿⡸⠁⢸⡾⢦⡀⠳⡀⠹⡽⣿⣿⣿⣷⠍⠻⢿⣿⣶⣤⣀⠀⠀⠈⢳⣝⢿⡆⢴⣿⣿⣷⡄⢸⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⡇⠸⡇⡇⠀⢾⡀⣀⡷⠰⣧⠀⡇⣹⣿⣿⣿⣄⣠⣶⣭⡻⢿⣿⣷⠄⠀⠈⠻⢧⡀⠈⢻⡿⠋⠀⣼⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⡇⢀⣷⢻⣄⠀⠉⠉⠀⢀⡿⢠⡇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣝⢿⣷⣄⠀⠀⠀⢻⣀⣾⣷⣄⠀⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⡇⠘⣿⣷⢉⣓⠶⠾⣴⣾⠃⢸⠇⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠹⣿⣦⠀⠀⢈⣇⣿⣿⡿⠁⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⡇⠀⢈⣴⣿⣿⣿⣿⢹⣿⠀⢿⢠⣾⣿⣿⣿⣿⡿⣿⣿⣿⣿⣷⣄⣤⢻⣿⠀⠀⠸⣿⣹⣯⠀⠀⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⡇⢰⣿⣿⣿⣿⡿⠿⢸⣿⠀⢸⣼⣿⣿⣿⠿⣫⣾⣮⠻⣿⣿⣿⣿⡿⣼⣿⠀⢠⣀⡇⣿⣿⣷⡄⣿⣿⣿⣿⡁⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⡇⢸⣿⣿⣿⣿⡅⢀⢸⣿⠀⠸⣷⡻⠟⠁⠀⠹⣿⠋⠀⠈⠻⣿⠟⣱⣿⠏⢀⣿⣿⠁⣿⣿⣿⡇⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⡇⢸⣿⣿⣿⣿⣿⣿⡜⣿⣧⡀⠉⢹⢤⡀⠀⣴⣿⣦⠀⠀⣀⣴⡾⠟⠀⢀⣾⣿⣇⣰⣿⣿⣿⡇⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣷⡘⠻⢿⣿⣿⣿⣿⣿⣼⣿⣿⣶⡀⠀⠉⠛⠒⠒⠶⠟⠛⠋⠉⢀⣠⣶⣿⣿⣿⣿⣿⡿⢟⣫⣵⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣷⣦⣈⠙⠻⠿⠟⣱⣦⣍⡻⢿⣿⣿⣿⣆⣠⣶⣦⣤⣴⡾⠿⠛⣋⡻⣿⠿⣋⣥⣾⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣀⠙⠻⠿⠛⢷⣶⣬⡭⣭⣭⣭⡭⣥⣴⣶⠊⠻⢟⣫⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠋⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠻⢿⣿⣿⣿⣿⣿⣿⣶⣤⣀⠈⠻⡏⠀⢘⣿⣟⠀⠈⡿⣋⣤⣶⣿⣿⣿⣿⣿⣿⣿⠿⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣄⡚⠿⢟⣠⣴⣾⣿⣿⣿⣿⣿⣿⣿⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠿⣿⡿⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        """)
 
 
-#Funktion för tredje svaret
-
-
-#Funktion för att få till en konversation med chatbot
+# Funktion för att få till en konversation med chatbot
 def chat():
-    #Anropar metod för första matchningen
+    # Anropar metod för att användaren ska mata in sitt namn
+    name = get_name("What is your name? ")
+    if name is None:
+        return
+
+    # Anropar metod matchningarna
     first_matcher()
     second_matcher()
-    #Anropar metod för att användaren ska mata in sitt namn
-    get_name("What is your name? ")
-    #
+    third_matcher()
+
     doc = get_user_response("How did you come by my app? ")
-    #
+    if doc is None:
+        return
     response = respond_to_first_reply(doc)
-
-    response = respond_to_second_reply(doc)
-
-    response = respond_to_third_reply(doc)
-
     print("Tom Riddle: ", response)
 
+    doc = get_user_response("Would you like to know more? ")
+    if doc is None:
+        return
+    response = respond_to_second_reply(doc)
+    print("Tom Riddle: ", response)
 
-
-
-chat()
-
-
-
-
-
+    doc = get_user_response("But I know who it was last time... ")
+    if doc is None:
+        return
+    response = respond_to_third_reply(doc)
+    print("Tom Riddle: ", response)
--- readme.txt
+++ readme.txt
@@ -3,22 +3,30 @@ Tom Riddle's Diary Chatbot
 Baserad på boken, Harry Potter and the Chamber of Secrets av J.K. Rowling
 Textutdrag som används som bas för appen finns i filen "Harry_Potter.txt"
 
-Öppna i PyCharm, ladda ner Jupyter notebooks och SpaCy
+Öppna i PyCharm, ladda ner SpaCy och PhraseMatcher
 
 I terminalen, skriv följande för att installera det stora Engelska språkpaketet:
 python -m spacy download en_core_web_lg
 
-Kör programmet och svara på frågorna som ställs. För mer information om frågorna och möjliga svar, se nedan.
+Kör programmet från filen "chatbot.py" och svara på frågorna som ställs av chatboten.
+Det rekommenderas att du drar ut terminalen så att den tar mycket plats under tiden som programmet körs.
+Konversationen ska efterlikna den som sker mellan Harry Potter och Tom Riddle, via skrift i en fysisk dagbok, se .txt-
+filen för utdrag från boken.
+
+För mer information om frågorna och möjliga svar, se facit. Annars är tanken att du bor kunna hänga med i botens flöde.
+
+En förklaring av programmet:
 
 Boten kommer att börja med att be om ditt namn. Om det inmatade värdet inte känns igen som ett namn (entitet: person)
 så kommer du att få en uppmaning att skriva in ett korrekt namn tills dess att det känns igen som ett riktigt namn.
+OBS att SpaCy inte är särskilt tillförlitlig när det kommer till att kunna identifiera namn korrekt då den ibland
+reagerar på påhittade namn som om de vore riktiga och vice versa.
 
-Du får sedan frågan från boten om hur du hittade appen. Ord som godkänns är "online", "github", eller "computer". Du kan
-skriva meningar som innehåller orden för att de ska matchas eller så skriver du endast ett av orden ovan. Om det inte
-matchas så får du ett lämpligt meddelande.
+Du får sedan frågan från boten om hur du hittade appen. Du kan skriva svar som består av ett eller flera ord i en
+mening. Om det inte matchas så får du ett lämpligt meddelande med möjlighet att fortsätta konversationen med chatbot.
 
-Boten kommer sedan att fråga om du vill veta mer om de händelser som skedde på skolan. Godkända ord som svar på detta är
-"yes", "tell me" och "why". Använder du någon av dessa så får du en historia om vad som hände med the Chamber of Secrets
-förra gången.
+Boten kommer sedan att fråga om du vill veta mer om de händelser som skedde på skolan.
+
+Du kan när som helst avsluta programmet under körning genom att mata in ett av fyra ord (se facit).
 
 
